02b8645716dd6065678cbcc5b4be3705af321b39
ranjansharma-ui <ranjankumar4278907@gmail.com>
2024-11-01 10:07:37 +0000

    Data type

---
+++ javascript/datatype.js
@@ -0,0 +1,33 @@
+// Reference (Non primitive)
+
+// Array, Object, Functions
+
+const score = 100
+const scoreValue = 100.3
+
+let userEmail;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 345325342532453n
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "Ranjan",
+    age: 22,
+
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+    
+
+}
+console.log(typeof myFunction);
+console.log(typeof id);
+
+
+
+
