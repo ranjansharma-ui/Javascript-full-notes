449d1a08fed0ca00fad594634ddfeec785dbe1d9
ranjansharma-ui <ranjankumar4278907@gmail.com>
2024-11-02 14:34:19 +0000

    scope

---
+++ function_and_parameters/scope.js
@@ -0,0 +1,58 @@
+//var c = 300
+let a = 300
+
+if(true){
+    let a = 10
+    const b = 20
+    
+
+}
+//console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+
+function one(){
+    const username = "Ranjan"
+    function two(){
+        const Website = "youtube"
+        //console.log(username);
+        
+    }
+    // console.log(Website); error
+
+    two()
+    
+}
+
+//one()
+
+if (true) {
+    const username = "hitesh"
+    if(username === "hitesh") {
+        const Website = "Instagram"
+        //console.log(username + Website)
+    }
+    // console.log(Website);
+    
+}
+// console.log(username);
+
+
+// ++++++++++++++++++++++ intersting ++++++++++++++
+console.log(addone(5));
+function addone(num){
+    return num + 1
+
+}
+
+// console.log(addone(5));
+
+//console.log(addTwo(5)); // error
+const addTwo = function(num){
+    return num + 2;
+}
+
+// console.log(addTwo(5));
