f0c2c32ad84a69b610ad8f726c4655dda267bba4
ranjansharma-ui <ranjankumar4278907@gmail.com>
2024-11-02 08:22:29 +0000

    Json Api

---
+++ javascript/json_Api.js
@@ -0,0 +1,13 @@
+// {
+//     "name": "Ranjan",
+//     "course": "cs",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+// json frommter
+// i learn to copy  use and view  api format using json fromtter 
