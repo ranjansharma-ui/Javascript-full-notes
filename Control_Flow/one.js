7c236b8af95b3d685124f5662c6988cdd5e823ba
ranjansharma-ui <ranjankumar4278907@gmail.com>
2024-11-03 05:28:28 +0000

    control Statement

---
+++ Control_Flow/one.js
@@ -0,0 +1,82 @@
+// if
+const isUserloggedIn = true
+const temparature = 41
+if (temparature < 50){
+    // console.log("less than 50");
+    
+
+} else{
+    console.log("temparature is greater than 50");
+}
+
+
+
+// <,>,<=,=<, ==, !=, ===(sick equal ), !==
+
+// const score = 200;
+// if (score > 100){
+//     let power = "fly";
+//     console.log(`User power : ${power}`);
+// }
+
+
+
+const balance = 1000
+
+// if (balance > 500) console.log("Test");
+
+//nesting
+// if(balance < 500) {
+//     console.log("less than");
+// } else if(balance < 740){
+//     console.log("less than 750");
+// } else{
+//     console.log("less than 1200");
+    
+// }
+
+// const isUserlogged = true
+// const debitCard = true
+// const loggedInFromGoogle = false
+// const loggedInFromEmail = true
+
+// if (isUserlogged && debitCard) {
+//     console.log("Allow to buy course");
+    
+// }
+
+// if ( loggedInFromEmail || loggedInFromGoogle){
+//     console.log("User logged in");
+    
+// }
+
+//++++++++++++++++++++++++++++++ Switch Statement ++++++++++++++++
+// syntax
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+
+    default:
+        console.log("default case math");
+        
+        break;
+}
+
