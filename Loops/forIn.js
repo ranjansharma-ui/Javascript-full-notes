bf8fe6a04c096c4589df41aadc1483a37468f7df
ranjansharma-ui <ranjankumar4278907@gmail.com>
2024-11-03 06:59:18 +0000

    for in

---
+++ Loops/forIn.js
@@ -0,0 +1,29 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+    
+}
+
+const programming = ["js","java","cpp","py"]
+
+for (const key in programming) {
+    // console.log(programming[key]);
+    
+}
+
+// const map = new Map() // unique
+// map.set('delhi',"de")
+// map.set('Usa',"U")
+// map.set('bihar',"b")
+// map.set('delhi',"de")
+
+// for (const key in map) {
+//   console.log(key);
+  
+// } // not itterable
