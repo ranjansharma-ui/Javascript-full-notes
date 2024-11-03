b6ebd5121d8945c5ff41543a84866ddb81f77b28
ranjansharma-ui <ranjankumar4278907@gmail.com>
2024-11-03 06:30:34 +0000

    while loop

---
+++ Loops/while.js
@@ -0,0 +1,20 @@
+let i = 0
+while (i <= 10) {
+    console.log(`value of index is ${i}`);
+    i += 2;
+    
+}
+
+
+let myArray = ["spider", "krish", "hulk", "thor"]
+let j = 0;
+while (myArray.length > j){
+    console.log(myArray[j]);
+    j++
+}
+
+let score = 1
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score <= 10);
